e5409d19b110e5b414a0a9c5084819bfffe89e7d
slify08 <islimaxmc@gmail.com>
2021-01-26 17:12:48 +0800

    udpate

--- app.js
+++ app.js
@@ -8,17 +8,35 @@ $(window).on("load",function(){
 var home = $('.name');
 
 home.click(() => {
-    home.addClass('animate__animated animate__headShake');
-
-    setTimeout(() => {
-        home.removeClass('animate__animated animate__headShake');
-    }, 500);
+    home.animate(() => {
+        bottom: '500px'
+    });
 });
 
 var tab = $('.tab').hide();
 var menu = $('.menu');
 
 menu.click(() => {
-    tab.slideToggle(1500);
-    menu.toggleClass('active');
+    menu.fadeOut(500);
+    setTimeout(() => {
+        tab.slideDown(1500);
+    }, 500);
+});
+
+var close = $('.close');
+
+close.click(() => {
+    tab.slideUp(1500);
+    setTimeout(() => {
+        menu.fadeIn(500);
+    }, 1500);
+});
+
+
+$('.home').click(() => {
+    $('.home').addClass('animate__animated animate__headShake');
+
+    setTimeout(() => {
+        $('.home').removeClass('animate__animated animate__headShake');
+    }, 500);
 });
